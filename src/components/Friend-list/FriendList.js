ef9e8f6aabd81c97d5abf30f51e805988a0be26a
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-10-06 02:53:42 +0300

    upd

--- src/components/Friend-list/FriendList.js
+++ src/components/Friend-list/FriendList.js
@@ -40,11 +40,3 @@ FriendList.propTypes = {
     }),
   ),
 };
-
-// {/* <ul class="friend-list">
-//   <li class="item">
-//   <span class="status"></span>
-//   <img class="avatar" src="" alt="" width="48" />
-//   <p class="name"></p>
-// </li>
-// </ul> */}
